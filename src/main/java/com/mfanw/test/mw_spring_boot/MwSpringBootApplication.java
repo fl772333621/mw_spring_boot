9d966dea061dafa87e49d070c712b0c416afbad6
孟伟 <772333621@qq.com>
2018-03-07 13:39:52 +0800

    add spring boot from https://start.spring.io/

---
+++ src/main/java/com/mfanw/test/mw_spring_boot/MwSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.mfanw.test.mw_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwSpringBootApplication.class, args);
+	}
+}
