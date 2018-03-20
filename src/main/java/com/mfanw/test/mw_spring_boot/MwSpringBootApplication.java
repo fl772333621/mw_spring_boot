a7df0b3028faafa299ceee958c1c0218324e5444
孟伟 <772333621@qq.com>
2018-03-20 18:52:10 +0800

    添加druid、mysql等配置。

--- src/main/java/com/mfanw/test/mw_spring_boot/MwSpringBootApplication.java
+++ src/main/java/com/mfanw/test/mw_spring_boot/MwSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.mfanw.test.mw_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan // 添加该行才会主动去搜索servlet和filter配置
 public class MwSpringBootApplication {
 
 	public static void main(String[] args) {
